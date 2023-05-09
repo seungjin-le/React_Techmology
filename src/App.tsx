282ea39bee32b7bc30481d703444da12502b5b95
Seungjin Lee <dltmdwls154@gmail.com>
2023-05-09 21:58:20 +0900

    Eslint 셋팅

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,17 @@ import Main from 'pages/home/Main'
 
 const loading = <div>asd</div>
 
+const Page404 = React.lazy(() => import('pages/page404/Page404'))
+const Page500 = React.lazy(() => import('pages/page500/Page500'))
+
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
         <Suspense fallback={loading}>
           <Routes>
+            <Route path='/404' element={<Page404 />} />
+            <Route path='/500' element={<Page500 />} />
             <Route path='/*' element={<Main />} />
           </Routes>
         </Suspense>
---
+++ src/pages/page500/Page500.tsx
@@ -0,0 +1,9 @@
+const Page500 = () => {
+  return (
+    <div>
+      <h1>Error 500</h1>
+    </div>
+  )
+}
+
+export default Page500
